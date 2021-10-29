fd56bb3f7c2bd08debf879a65774caf461f1092e
schoezb <schoezb@uoc.edu>
2021-10-29 17:04:21 +0200

    Comentaris

--- src/main.py
+++ src/main.py
@@ -12,23 +12,36 @@ import requests
 # ---------------------------------------------------------------------------
 # Functions
 # ---------------------------------------------------------------------------
-def accessTopMostPopularAninme(url):
+def getmostpopularaninme(url: str):
+    # Retorna el contingut de la pàgina web
     page = requests.get(url)
+    # Organitzem el  contingut de la pàgina web
     soup = BeautifulSoup(page.content, 'html.parser')
+    # Busquem la secció de Top Anime
     menuAnime = soup.find_all(text="Top Anime", href=True)
+    # Extraiem la URL
     topAnimeUrl = menuAnime[0]['href']
-    print(topAnimeUrl)
+    # Extraiem el contigut de la nova URL
     pageTopAnime = requests.get(topAnimeUrl)
+    # Organitzem el  contingut de Top Anime
     soupTopAnime = BeautifulSoup(pageTopAnime.content, 'html.parser')
+    # Busquem la secció de Most Popular
     mostPopular = soupTopAnime.find_all(text="Most Popular", href=True)
-    mostPopularUrl= mostPopular[0]['href']
+    # Extraiem la URL
+    mostPopularUrl = mostPopular[0]['href']
+    # Extraiem la URL de la pàgina Top Anime
     root = topAnimeUrl.split("?")[0]
-    pageMostPopular = requests.get(root+mostPopularUrl)
+    # Extraiem el contigut de la URL de Most Popular Anime
+    pageMostPopular = requests.get(root + mostPopularUrl)
+    # Organitzem el  contingut de la pàgina web
     soupMostPopular = BeautifulSoup(pageMostPopular.content, 'html.parser')
-    print(soupMostPopular.contents)
+
+    # Retornem el contigut amb el qual generarem el dataset
+    return soupMostPopular.contents
+
 
 # ---------------------------------------------------------------------------
 # Main
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
-    accessTopMostPopularAninme("https://myanimelist.net/")
+    getmostpopularaninme("https://myanimelist.net/")
